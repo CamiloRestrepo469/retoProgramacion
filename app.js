3d2a5e69dad18325b26176b801d65a42b2120845
CamiloRestrepo469 <rsgermancamilo@gmail.com>
2021-03-02 07:33:32 -0500

    fin del reto

--- app.js
+++ app.js
@@ -6,24 +6,44 @@ de 5 por “Buzz”. Para los números que, al tiempo, son múltiplos de 3 y 5, 
 “FizzBuzz*/
 
 var text = "";
+var text1 = "";
+var text2 = "";
+var text3 = "";
 var i;
 "<h2>el numero enteros</h2> "
-for (i = 0; i < 200; i++) {
-    if (i === 200) { break; }
+for (i = 0; i <= 201; i++) {
+    if (i === 201) { break; }
     text += "el numero enteros " + i + "<br>";
   }
   "<br>"
   "<hr>"
 
-for (i = 0; i < 200; i=i+3) {
+for (i = 0; i <= 200; i=i+3) {
   if (i === 200) { break; }
-  text += "el numero es multiplos de tres Fizz " + i + "<br>";
+  text1 += "el numero es multiplos de tres Fizz " + i + "<br>";
 }
 "<br>"
 "<hr>"
 
-for (i = 0; i < 200; i=i+5) {
+for (i = 0; i <= 200; i=i+5) {
     if (i === 200) { break; }
-    text += "el numero es multiplos de tres Buzz " + i + "<br>";
+    text2 += "el numero es multiplos de tres Buzz " + i + "<br>";
   }
-document.getElementById("demo").innerHTML = text;
+  for (i = 0; i <= 200; i=i+3*5) {
+    if (i === 200) { break; }
+    text3 += "el numero es multiplos de tres y de cinco  FizzBuzz " + i + "<br>";
+  }
+var demo = document.getElementById("demo");
+demo.innerHTML += `
+    <div class container>
+    <h1>Numeros enteros</h1>
+    <h3>${text}</h3>
+    <h1>Numeros multiplos de tres </h1>
+    <h3>${text1}</h3>
+    <h1>Numeros multiplos de cinco</h1>
+    <h3>${text2}</h3>
+    <h1>Numeros multiplos de tres y de cinco</h1>
+    <h3>${text3}</h3>
+    </div>
+
+`;
